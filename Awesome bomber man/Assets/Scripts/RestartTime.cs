f966bddb95efaa0dd1a95ca5eb76f5a5c9e6d3c2
monkeyhammer42 <michael-whiddett@hotmail.com>
2019-11-01 11:47:45 +0000

    Full game commit

---
+++ Awesome bomber man/Assets/Scripts/RestartTime.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartTime : MonoBehaviour
+{
+    public void RestartGame() {
+        SceneManager.LoadScene("Gameplay");
+        Time.timeScale = 1f;
+        }
+    void start() { }
+    void Update()
+    {
+
+    }
+
+
+}
